c7c19a3cb8d793fad46f9882f5b73f6a1d789c11
Rushikesh Deo <rushikesh.deo@netcracker.com>
2023-05-31 15:31:48 -0400

    test

---
+++ openrc.sh
@@ -0,0 +1,28 @@
+export OS_AUTH_URL=https://zephyr02cn.netcracker.com:5000
+# With the addition of Keystone we have standardized on the term **project**
+# as the entity that owns the resources.
+export OS_PROJECT_ID=fc60e90d60954ce7aff9c42261c76b75
+export OS_PROJECT_NAME="RUDE0423"
+export OS_USER_DOMAIN_NAME="netcracker"
+if [ -z "$OS_USER_DOMAIN_NAME" ]; then unset OS_USER_DOMAIN_NAME; fi
+export OS_PROJECT_DOMAIN_ID="dd8f1c24c3f2422a8b87d45e60b2035c"
+if [ -z "$OS_PROJECT_DOMAIN_ID" ]; then unset OS_PROJECT_DOMAIN_ID; fi
+# unset v2.0 items in case set
+unset OS_TENANT_ID
+unset OS_TENANT_NAME
+# In addition to the owning entity (tenant), OpenStack stores the entity
+# performing the action as the **user**.
+export OS_USERNAME="rude0423"
+# With Keystone you pass the keystone password.
+#echo "Please enter your OpenStack Password for project $OS_PROJECT_NAME as user $OS_USERNAME: "
+#read -sr OS_PASSWORD_INPUT
+#export OS_PASSWORD=U2FsdGVkX18vYJrO2qtJLNoJFrbcoy91idL+Qq/AugQ=
+export OS_PASSWORD="Lokmat@23"
+# If your configuration has multiple regions, we set that information here.
+# OS_REGION_NAME is optional and only valid in certain environments.
+export OS_REGION_NAME="RegionOne"
+# Don't leave a blank variable, unset it if it was empty
+if [ -z "$OS_REGION_NAME" ]; then unset OS_REGION_NAME; fi
+export OS_INTERFACE=public
+export OS_IDENTITY_API_VERSION=3
+export OS_CACERT=zephyr02.crt
