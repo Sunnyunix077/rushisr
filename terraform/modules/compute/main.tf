ee58b0cbff8e8ee1892e4284c9a6588c97829e41
Rushikesh Deo <rushikesh.deo@netcracker.com>
2023-06-16 03:13:17 -0400

    Test

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -14,8 +14,8 @@ resource "openstack_compute_instance_v2" "test-instance" {
   count           = var.instance_count
   name            = format(
     "%s%02d%s",
-    local.instance_prefix[count.index / 3],
-    (count.index % 3) + 1,
+    local.instance_prefix[(count.index % length(local.instance_prefix))],
+    ((count.index / length(local.instance_prefix)) % 3) + 1,
     var.instance_suffix != "" ? var.instance_suffix : ""
   )
   flavor_name     = var.instance_flavor
