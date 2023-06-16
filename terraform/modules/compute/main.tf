12b755e35432851c4cab624d82fbc0edd5ebeb97
Rushikesh Deo <rushikesh.deo@netcracker.com>
2023-06-16 14:37:59 -0400

    Test

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -15,6 +15,12 @@ locals {
     "labcr",
     "labst",
   ]
+  instance_flavors = {
+    "labdpl" = "IaaS.Vcpu_2.ram_4.disk_40"
+    "labcm"  = "IaaS.Vcpu_2.ram_4.disk_40"
+    "labcr"  = "IaaS.Vcpu_2.ram_14.disk_40"
+    "labst"  = "IaaS.Vcpu_2.ram_4.disk_40"
+  }
 }
 # Create a new instance
 resource "openstack_compute_instance_v2" "test-instance" {
@@ -26,7 +32,7 @@ resource "openstack_compute_instance_v2" "test-instance" {
     count.index < 4 ? 1 : floor((count.index - 4) / 3) + 2,
     var.instance_suffix != "" ? var.instance_suffix : ""
   )
-  flavor_name     = element(var.instance_flavors, count.index)
+  flavor_name     = local.instance_flavors[local.instance_prefixes[count.index < 4 ? count.index : (count.index % 3) + 1]]
   key_pair        = var.keypair_name
   security_groups = [var.sg_id]
   image_name      = var.instance_image
