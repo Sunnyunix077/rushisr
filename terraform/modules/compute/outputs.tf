2f53e6bfcf7039365cc7b11a8719c1084ae3ef7e
Rushikesh Deo <rushikesh.deo@netcracker.com>
2023-06-16 15:21:27 -0400

    Test

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -4,3 +4,9 @@ output "instance_ip" {
 output "instance_id" {
   value = openstack_compute_instance_v2.test-instance.*.id
 }
+
+output "instance_flavors" {
+  description = "Flavors of the created instances"
+  value       = module.compute.openstack_compute_instance_v2[*].flavor_name
+}
+
